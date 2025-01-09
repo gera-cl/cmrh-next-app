2007ff9690164ea51525f8f6befd3e1237449a9f
gera-cl <hernandezf.german@gmail.com>
2025-01-09 16:25:44 -0300

    Initial commit

---
+++ app/providers.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import type { ThemeProviderProps } from "next-themes";
+
+import * as React from "react";
+import { NextUIProvider } from "@nextui-org/system";
+import { useRouter } from "next/navigation";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+import { SessionProvider } from "next-auth/react"
+import { Session } from "next-auth";
+
+export interface ProvidersProps {
+  children: React.ReactNode;
+  themeProps?: ThemeProviderProps;
+  session: Session
+}
+
+declare module "@react-types/shared" {
+  interface RouterConfig {
+    routerOptions: NonNullable<
+      Parameters<ReturnType<typeof useRouter>["push"]>[1]
+    >;
+  }
+}
+
+export function Providers({ children, themeProps, session }: ProvidersProps) {
+  const router = useRouter();
+
+  return (
+    <SessionProvider session={session}>
+      <NextUIProvider navigate={router.push}>
+        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      </NextUIProvider>
+    </SessionProvider>
+  );
+}
