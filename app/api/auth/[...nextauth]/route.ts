615c4a3ff22f067f561baefa8c3a4411659c619a
gera-cl <hernandezf.german@gmail.com>
2025-01-10 12:59:22 -0300

    save new users in db

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { createUser, getUserByEmail } from '@/db/queries/users.queries';
 
 const handler = NextAuth({
   providers: [
@@ -8,6 +9,27 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account?.provider === "google") {
+        console.log('account', account);
+        console.log('profile', profile);
+        const email = profile?.email;
+        const user = await getUserByEmail(email!);
+
+        // If user isn't created, create user
+        if (user.length === 0) {
+          const createdUser = await createUser({
+            name: profile?.name!,
+            email: profile?.email!,
+          })
+          return createdUser ? true : false;
+        }
+        return true;
+      }
+      return false;
+    },
+  }
 });
 
 export { handler as GET, handler as POST };
