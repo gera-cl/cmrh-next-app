133acc5eaeba44cd1465071769a87e4c630fae48
Germán Hernández F <gmhernandez@falabella.cl>
2025-01-14 17:34:12 -0300

    update navbar

---
+++ app/credentials/new/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import React from "react";
+import { Button } from "@nextui-org/button";
+import { Form } from "@nextui-org/form";
+import { Input, Textarea } from "@nextui-org/input";
+import { Divider } from "@nextui-org/divider";
+
+export default function NewCredentialPage() {
+  const [action, setAction] = React.useState<null | string>(null);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.currentTarget));
+
+    await fetch("/api/credentials", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setAction(`submit ${JSON.stringify(data)}`);
+  };
+
+  return (
+    <Form
+      className="w-full max-w-xs flex flex-col gap-4"
+      validationBehavior="native"
+      onReset={() => setAction("reset")}
+      onSubmit={handleSubmit}
+    >
+      <span className="text-xl">New credential</span>
+      <Input
+        isRequired
+        errorMessage="Please enter a valid url"
+        label="URL"
+        labelPlacement="outside"
+        name="url"
+        placeholder="Enter the url"
+        type="url"
+      />
+
+      <Input
+        isRequired
+        errorMessage="Please enter a valid name"
+        label="Name"
+        labelPlacement="outside"
+        name="name"
+        placeholder="Enter the credential name"
+        type="text"
+      />
+
+      <Input
+        isRequired
+        errorMessage="Please enter a valid username"
+        label="Username"
+        labelPlacement="outside"
+        name="username"
+        placeholder="Enter your username"
+        type="text"
+      />
+
+      <Input
+        isRequired
+        errorMessage="Please enter a valid password"
+        label="Password"
+        labelPlacement="outside"
+        name="password"
+        placeholder="Enter the password"
+        type="password"
+      />
+
+      <Divider className="my-4" />
+
+      <Input
+        errorMessage="Please enter a valid alternative username"
+        label="Alternative username"
+        labelPlacement="outside"
+        name="alternative_username"
+        placeholder="Enter an alternative username"
+        type="text"
+      />
+
+      <Textarea
+        className="max-w-xs"
+        label="Note"
+        labelPlacement="outside"
+        name="note"
+        placeholder="Enter a note"
+      />
+
+      <div className="flex gap-2">
+        <Button color="primary" type="submit">
+          Submit
+        </Button>
+        <Button type="reset" variant="flat">
+          Reset
+        </Button>
+      </div>
+      {action && (
+        <div className="text-small text-default-500">
+          Action: <code>{action}</code>
+        </div>
+      )}
+    </Form>
+  );
+}
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useSession, signOut } from "next-auth/react"
 import {
   Navbar as NextUINavbar,
   NavbarContent,
@@ -13,6 +16,8 @@ import { Link } from "@nextui-org/link";
 import { Input } from "@nextui-org/input";
 import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
+import { Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger } from "@nextui-org/dropdown";
+import { Avatar } from "@nextui-org/avatar";
 import clsx from "clsx";
 
 import { siteConfig } from "@/config/site";
@@ -25,28 +30,76 @@ import {
   SearchIcon,
   Logo,
 } from "@/components/icons";
+import { PiPlusCircleDuotone } from "react-icons/pi";
+
+import { Session } from "next-auth";
+import { User } from "@nextui-org/user";
+
+const userMenu = (session: Session | null, status: "authenticated" | "loading" | "unauthenticated") => (
+  <Dropdown placement="bottom-end">
+    <DropdownTrigger>
+      <Avatar
+        isBordered
+        as="button"
+        className="transition-transform"
+        radius="lg"
+        name={session?.user.name}
+        size="sm"
+        src={session?.user.image}
+      />
+    </DropdownTrigger>
+    <DropdownMenu aria-label="Profile Actions" variant="flat">
+      <DropdownSection showDivider>
+        <DropdownItem key="profile" textValue="user">
+          <User
+            avatarProps={{
+              src: session?.user.image,
+            }}
+            description={
+              <div className="max-w-28 break-words">
+                {session?.user.email}
+              </div>
+            }
+            name={session?.user.name}
+            className=""
+          />
+        </DropdownItem>
+      </DropdownSection>
+
+      <DropdownSection >
+        <DropdownItem key="settings">Profile</DropdownItem>
+        <DropdownItem key="configurations">Settings</DropdownItem>
+        <DropdownItem key="logout" color="danger" onPress={() => signOut()}>
+          Log Out
+        </DropdownItem>
+      </DropdownSection>
+    </DropdownMenu>
+  </Dropdown>
+)
+
+const searchInput = (
+  <Input
+    aria-label="Search"
+    classNames={{
+      inputWrapper: "bg-default-100",
+      input: "text-sm",
+    }}
+    endContent={
+      <Kbd className="hidden lg:inline-block" keys={["command"]}>
+        K
+      </Kbd>
+    }
+    labelPlacement="outside"
+    placeholder="Search..."
+    startContent={
+      <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+    }
+    type="search"
+  />
+);
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
+  const { data: session, status } = useSession()
 
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
@@ -54,10 +107,10 @@ export const Navbar = () => {
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <p className="font-bold text-inherit">CUMORAH</p>
           </NextLink>
         </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
+        <ul className="hidden sm:flex gap-4 justify-start ml-2">
           {siteConfig.navItems.map((item) => (
             <NavbarItem key={item.href}>
               <NextLink
@@ -73,9 +126,18 @@ export const Navbar = () => {
             </NavbarItem>
           ))}
         </ul>
+        <Button
+          as={Link}
+          className="hidden sm:flex ml-2 text-sm font-semibold bg-slate-100 text-slate-700"
+          href={siteConfig.links.newCredential}
+          startContent={<PiPlusCircleDuotone className="w-5 h-5" />}
+          variant="solid"
+        >
+          Add Credential
+        </Button>
       </NavbarContent>
 
-      <NavbarContent
+      {/* <NavbarContent
         className="hidden sm:flex basis-1/5 sm:basis-full"
         justify="end"
       >
@@ -104,13 +166,22 @@ export const Navbar = () => {
             Sponsor
           </Button>
         </NavbarItem>
+      </NavbarContent> */}
+
+      <NavbarContent
+        className="hidden sm:flex"
+        as="div"
+        justify="end"
+      >
+        {userMenu(session, status)}
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
+        <Link aria-label="New Credential" href={siteConfig.links.newCredential}>
+          <PiPlusCircleDuotone className="text-default-500 w-6 h-6" />
         </Link>
         <ThemeSwitch />
+        {userMenu(session, status)}
         <NavbarMenuToggle />
       </NavbarContent>
 
@@ -120,14 +191,8 @@ export const Navbar = () => {
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
               <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
+                href={item.href}
+                color="foreground"
                 size="lg"
               >
                 {item.label}
