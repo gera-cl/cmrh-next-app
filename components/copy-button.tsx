92eb17e5df11c1d07ca68db77e0bfedbbb0632dd
Germán Hernández F <gmhernandez@falabella.cl>
2025-01-22 14:02:24 -0300

    replace react-copy-to-clipboard with native fn

--- components/copy-button.tsx
+++ components/copy-button.tsx
@@ -1,7 +1,6 @@
 import { Tooltip } from "@heroui/tooltip";
 import { Button } from "@heroui/button"
 import { FC, ComponentProps, useState } from "react"
-import CopyToClipboard from "react-copy-to-clipboard"
 import { IoCheckmarkDoneSharp } from "react-icons/io5";
 
 type CopyButtonProps = {
@@ -15,7 +14,8 @@ type CopyButtonProps = {
 export const CopyButton = (props: CopyButtonProps) => {
   const [isCopied, setIsCopied] = useState<boolean>(false)
 
-  const handleCopy = () => {
+  const handleCopy = async () => {
+    await navigator.clipboard.writeText(props.textToCopy);
     setIsCopied(true);
     setTimeout(() => {
       setIsCopied(false);
@@ -24,18 +24,21 @@ export const CopyButton = (props: CopyButtonProps) => {
 
   return (
     <Tooltip content={props.ariaLabel}>
-      <Button isIconOnly aria-label={props.ariaLabel} className={`${isCopied ? 'bg-green-500' : ''} ${props.className}`}>
-        <CopyToClipboard text={props.textToCopy} onCopy={() => handleCopy()}>
-          <div className="w-full h-full flex items-center justify-center">
-            {
-              isCopied ? (
-                <IoCheckmarkDoneSharp className="text-white h-5 w-5" />
-              ) : (
-                <props.icon className={`h-5 w-5 ${props.iconClassName || ''}`} />
-              )
-            }
-          </div>
-        </CopyToClipboard>
+      <Button
+        isIconOnly
+        aria-label={props.ariaLabel}
+        className={`${isCopied ? 'bg-green-500' : ''} ${props.className}`}
+        onPress={handleCopy}
+      >
+        <div className="w-full h-full flex items-center justify-center">
+          {
+            isCopied ? (
+              <IoCheckmarkDoneSharp className="text-white h-5 w-5" />
+            ) : (
+              <props.icon className={`h-5 w-5 ${props.iconClassName || ''}`} />
+            )
+          }
+        </div>
       </Button>
     </Tooltip>
   );
